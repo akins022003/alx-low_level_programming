2d2ca4bce583c394ca0aae7d4b7094bbff88c418
akins022003 <>
2022-12-19 10:05:39 -0800

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-		int i = 0;
+int i = 0;
 
-		for (; src[i] != '\0'; i++)
-		{
-		dest[i] = src[i];
-		}
-		return (dest);
+for (; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
